8b1df117f54f0c5e6d1d3a4580c4b53a53889711
BamBo <raksameth@gmail.com>
2017-06-30 19:14:37 +0700

    Add Two New Field Propertys For Store Model

--- app/src/main/java/com/thesis/brown/brown/store_list/ListStoreListModel.java
+++ app/src/main/java/com/thesis/brown/brown/store_list/ListStoreListModel.java
@@ -5,15 +5,21 @@ import java.io.Serializable;
 public class ListStoreListModel implements Serializable{
 
     private String title, road, phone, link, imgURL, openingTime;
+    private double longitude , latitude;
 
-    public ListStoreListModel(String title, String road, String phone, String link, String imgURL, String openingTime) {
+
+
+    public ListStoreListModel(String title, String road, String phone, String link, String imgURL, String openingTime, double longitude, double latitude) {
         this.title = title;
         this.road = road;
         this.phone = phone;
         this.link = link;
         this.imgURL = imgURL;
         this.openingTime = openingTime;
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
+
 
     public String getTitle() {
         return title;
@@ -38,4 +44,13 @@ public class ListStoreListModel implements Serializable{
     public String getOpeningTime() {
         return openingTime;
     }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
 }
